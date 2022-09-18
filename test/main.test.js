4156a1e599703d0ab398e06d581ec2e765557da3
Alejandro Uribe <alejandro@veryfi.com>
2022-09-18 00:52:13 -0500

    Add improvement for unit tests

--- test/main.test.js
+++ test/main.test.js
@@ -4,33 +4,23 @@
 
 const Client = require('../lib/main');
 const fs = require('fs');
+const {expect} = require("@jest/globals");
 const client_id = process.env.VERYFI_CLIENT_ID;
 const client_secret = process.env.VERYFI_CLIENT_SECRET;
 const username = process.env.VERYFI_USERNAME;
 const api_key = process.env.VERYFI_API_KEY;
-
-let doc_id =  'ID_TO_MODIFY';
-let delete_id = 'ID_TO_DELETE';
-
-let mockResponses = true; // Change to “false” if you want to test your personal credentials
+const base_url = process.env.VERYFI_URL;
+const api_version = "v7"
 
 
 //Creating the Client
-let veryfi_client = new Client(client_id, client_secret, username, api_key);
+let veryfi_client = new Client(client_id, client_secret, username, api_key, base_url, api_version);
+jest.setTimeout(100000)
 
 describe('Processing documents', () => {
-    jest.setTimeout(10000)
     test('Upload invoice for processing', async () => {
         try {
-            let response;
-            if (mockResponses) {
-                const processDocumentJson = require('./processDocument.json');
-                const veryfi_process_document = jest.fn();
-                veryfi_process_document.mockReturnValue(processDocumentJson);
-                response = veryfi_process_document();
-            } else {
-                response = await veryfi_client.process_document('test/receipt.png');
-            }
+            let response = await veryfi_client.process_document('test/receipt.png');
             expect(response['vendor']['name']).toBe('The Home Depot');
         } catch (error) {
             throw new Error(error);
@@ -39,22 +29,14 @@ describe('Processing documents', () => {
 
     test('Process document from buffer', async () => {
         try {
-            let response;
-            if (mockResponses) {
-                const processDocumentJson = require('./processDocument.json');
-                const process_document_buffer = jest.fn();
-                process_document_buffer.mockReturnValue(processDocumentJson);
-                response = process_document_buffer();
-            } else {
                 const file_path = 'test/receipt.png';
                 const image_file = fs.readFileSync(file_path, { encoding: 'base64' });
                 const base64_encoded_string = Buffer.from(image_file).toString('utf-8');
-                response = await veryfi_client.process_document_buffer(
+                let response = await veryfi_client.process_document_buffer(
                     base64_encoded_string,
                     'receipt.png'
                 );
-            }
-            expect(response['vendor']['name']).toBe('The Home Depot');
+            expect(response['total']).toBe(34.95);
         } catch (error) {
             throw new Error(error);
         }
@@ -62,16 +44,8 @@ describe('Processing documents', () => {
 
     test('Process document from URL', async () => {
         try {
-            let response;
-            if (mockResponses) {
-                const processDocumentUrlJson = require('./processDocumentUrl.json');
-                const veryfi_process_document_url = jest.fn();
-                veryfi_process_document_url.mockReturnValue(processDocumentUrlJson);
-                response = veryfi_process_document_url();
-            } else {
-                response = await veryfi_client.process_document_url('https://cdn.veryfi.com/receipts/92233902-c94a-491d-a4f9-0d61f9407cd2.pdf');
-            }
-            expect(response['vendor']['name']).toBe('Rumpke Waste & Recycling');
+            let response = await veryfi_client.process_document_url('https://cdn.veryfi.com/receipts/92233902-c94a-491d-a4f9-0d61f9407cd2.pdf');;
+            expect(response['vendor']['name']).toBe('Rumpke Of Ohio');
         } catch (error) {
             throw new Error(error);
         }
@@ -81,41 +55,18 @@ describe('Processing documents', () => {
 describe('Managing documents', () => {
     test('Get documents', async () => {
         try {
-            let docs;
-            if (mockResponses) {
-                const getDocumentsJson = require('./getDocuments.json');
-                const veryfi_get_documents = jest.fn();
-                veryfi_get_documents.mockReturnValue(getDocumentsJson);
-                docs = veryfi_get_documents();
-            } else {
-                docs = await veryfi_client.get_documents();
-            }
+            let docs = await veryfi_client.get_documents();
             expect(docs.length).toBeGreaterThan(0);
-            expect(docs).toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({
-                        'id': expect.any(Number),
-                        'category': expect.any(String)
-                    })
-                ])
-            );
         } catch (error) {
             throw new Error(error);
         }
     });
 
-    test(`Get document with id ${doc_id}`, async () => {
+    test(`Get document with id `, async () => {
         try {
-            let doc;
-            if (mockResponses) {
-                doc_id = 31727276;
-                const getDocumentJson = require('./getDocument.json');
-                const veryfi_get_document = jest.fn();
-                veryfi_get_document.mockReturnValue(getDocumentJson);
-                doc = veryfi_get_document();
-            } else {
-                doc = await veryfi_client.get_document(doc_id);
-            }
+            let docs = await veryfi_client.get_documents();
+            const doc_id = docs[0].id;
+            let doc = await veryfi_client.get_document(doc_id);
             expect(doc['id']).toBe(doc_id)
         } catch (error) {
             throw new Error(error);
@@ -125,17 +76,12 @@ describe('Managing documents', () => {
 
 describe('Editing Documents', () => {
     test('Update a document\'s entries', async () => {
-        let params = {'notes':'Note updated'};
+        const randomString = (Math.random() + 1).toString(36).substring(7);
+        let params = {'notes': randomString};
         try {
-            let response;
-            if (mockResponses) {
-                const updateDocumentJson = require('./updateDocument.json');
-                const veryfi_update_document = jest.fn();
-                veryfi_update_document.mockReturnValue(updateDocumentJson);
-                response = veryfi_update_document();
-            } else {
-                response = await veryfi_client.update_document(doc_id, params);
-            }
+            let docs = await veryfi_client.get_documents();
+            const doc_id = docs[0].id;
+            let response = await veryfi_client.update_document(doc_id, params);
             expect(response).toEqual(expect.objectContaining(params));
         } catch (error) {
             throw new Error(error);
@@ -144,16 +90,10 @@ describe('Editing Documents', () => {
 
     test('Delete a document by id', async () => {
         try {
-            let response;
-            if (mockResponses) {
-                const deleteDocumentJson = require('./deleteDocument.json');
-                const veryfi_delete_document = jest.fn();
-                veryfi_delete_document.mockReturnValue(deleteDocumentJson);
-                response = veryfi_delete_document();
-            } else {
-                response = await veryfi_client.delete_document(delete_id);
-            }
-            expect(response['status']).toBe('ok')
+            let docs = await veryfi_client.get_documents();
+            const doc_id = docs[0].id;
+            let response = await veryfi_client.delete_document(doc_id);
+            expect(response['status']).toBe(200)
         } catch (error) {
             throw new Error(error);
         }
