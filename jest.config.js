cb9c5d1bed5e18145e34106aa493c5be148e39c4
Alejandro Uribe <alejandro@veryfi.com>
2022-09-18 02:57:02 -0500

    Generate documentation

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-//import {defaults} from 'jest-config';
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  //moduleFileExtensions: [...defaults.moduleFileExtensions, 'd.ts’'],
 };
